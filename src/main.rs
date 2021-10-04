0fc11dcb9bbd6d271668fcb38b3ee7d4761edcaf
Julius Lungys <jl@nikulipe.com>
2021-10-04 16:11:21 +0300

    init route

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-mod counter;
-mod counter_hook;
-mod counter_old;
-mod counter_reducer;
-mod selection_listener;
+use yew_router::prelude::*;
+
+#[derive(Routable, Debug, Clone, PartialEq)] //Err the trait bound `SubRoute: FromStr` is not satisfied
+pub enum MyRoute {
+    #[at("/some/route/:sub_route")]
+    SubRoute { sub_route: SubRoute }, //Err the trait `std::fmt::Display` is not implemented for `SubRoute`
+}
+
+#[derive(Routable, Debug, Clone, PartialEq)]
+pub enum SubRoute {
+    #[at("/super/route")]
+    SubSubRoute,
+}
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-
-    // yew::start_app::<counter::Counter>();
-    // yew::start_app::<counter_old::CounterOld>();
-    // yew::start_app::<counter_reducer::CounterReducer>();
-    // yew::start_app::<counter_hook::CounterHook>();
-    yew::start_app::<selection_listener::SelectionListener>();
 }
