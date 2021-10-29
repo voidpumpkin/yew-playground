dcbfcffc8897f4ea4838432980cc0dd3ed2c59ef
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-10-29 16:59:57 +0300

    remove unused files and update rust edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,10 @@
-mod counter;
-// mod counter_hook;
-// mod counter_old;
-// mod counter_reducer;
 mod selection_listener;
-// pub mod utils;
 
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn start() {
-    // println!("hi");
-    // web_sys::console::log_1(&"log".into());
     wasm_logger::init(wasm_logger::Config::default());
-    // log::info!("test");
-    // yew::start_app::<counter::Counter>();
-    // yew::start_app::<counter_old::CounterOld>();
-    // yew::start_app::<counter_reducer::CounterReducer>();
-    // yew::start_app::<counter_hook::CounterHook>();
+
     yew::start_app::<selection_listener::SelectionListener>();
 }
