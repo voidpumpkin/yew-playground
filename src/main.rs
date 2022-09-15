4011337f966368fde79be4e6877d49aa8b9cb9fe
Julius Lungys <jl@nikulipe.com>
2022-09-15 12:30:56 +0300

    bug

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
+pub mod use_forever_suspension;
+
+use gloo::timers::callback::Timeout;
+use use_forever_suspension::use_forever_suspension;
 use yew::prelude::*;
 
 #[function_component]
+fn FirstPage() -> HtmlResult {
+    use_forever_suspension()?;
+
+    Ok(html! {
+        <button class="btn block">{ "Some button" }</button>
+    })
+}
+
+#[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+    let page_handle = use_state(|| 1);
+    let page = *page_handle;
+
+    use_memo(
+        move |()| {
+            Timeout::new(1_000, move || {
+                page_handle.set(2);
+            })
+        },
+        (),
+    );
+
+    use_memo(
+        |page| {
+            log::info!("🚀 {page}");
+        },
+        page,
+    );
 
     html! {
-        <div class="bg-blue-200">
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+        <>
+        <Suspense fallback={html! {<div>{"Loading..."}</div>}}>
+            if page == 1 {
+                // If uncommented will work
+                // <Suspense fallback={html! {<div>{"Loading 1..."}</div>}}>
+                    <FirstPage />
+                // </Suspense>
+            } else {
+                <h1>{"Page 2"}</h1>
+            }
+        </Suspense>
+        </>
     }
 }
 
